4afd7b3f4a90ce0a2a9feb4a0941fe3369995cbe
ewhiley <ejwhiley29@gmail.com>
2019-12-18 13:26:04 +1100

    Make greeting handle two names
    If there are two names in the database then the response will have the
    two names with an and in between.

--- src/com/company/World.java
+++ src/com/company/World.java
@@ -1,11 +1,29 @@
 package com.company;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
-public class World {
+class World {
 
     static String getGreeting(Date date, HashMap<Integer, String> people) {
-        return "Hi " + people.get(0) + ". The date and time is " + date;
+        if (people.size() == 1) {
+            return "Hi " + people.get(0) + ". The date and time is " + date;
+        } else {
+            return "Hi " + namesAsString(people) + ". The date and time is " + date;
+        }
+    }
+
+    private static String namesAsString(HashMap<Integer, String> people) {
+        ArrayList<String> names = new ArrayList<>(people.values());
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < names.size() - 1; i++ ) {
+            stringBuilder.append(names.get(i)).append(",");
+        }
+
+        return stringBuilder.toString()
+                .substring (0, stringBuilder.length()-1)
+                + " and "
+                + names.get(names.size() - 1);
     }
 
 }
