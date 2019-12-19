454a804cd0f128f7cb01b2cb282fced8a3672705
ewhiley <ejwhiley29@gmail.com>
2019-12-19 17:13:07 +1100

    Add User class
    So I can pass in a User to my UserStorage manager to be able to find
    what it needs.

--- src/com/company/world/storage/User.java
+++ src/com/company/world/storage/User.java
@@ -1,4 +1,19 @@
 package com.company.world.storage;
 
 public class User {
+    private Integer id;
+    private String name;
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
